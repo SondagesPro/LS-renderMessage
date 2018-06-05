91d20118c293b6715218cbfad5feffa3c97245d1
Denis Chenu <denis@sondages.pro>
2018-06-05 11:27:48 +0200

    [fix] Bad rendering for 2.6lts

--- views/2_06/public.php
+++ views/2_06/public.php
@@ -1,8 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $language; ?>" lang="<?php echo $language; ?>"<?php echo $dir; ?>>
-<head>
 <?php
-    echo templatereplace(file_get_contents("{$templateDir}/startpage.pstpl"));
     if($useCompletedTemplate)
     {
         echo templatereplace(file_get_contents("{$templateDir}/completed.pstpl"),array('COMPLETED'=>$message,'URL'=>''));
@@ -11,6 +7,4 @@
     {
         echo CHtml::tag("div",array('id'=>'wrapper','class'=>'message'),CHtml::tag("p",array("id"=>"tokenmessage"),templatereplace($message)));
     }
-    echo templatereplace(file_get_contents("{$templateDir}/endpage.pstpl"));
 ?>
-</body></html>
