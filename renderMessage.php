f7897ff2b547db46d62f6ef06c571b26e918a1eb
Denis Chenu <denis@sondages.pro>
2023-07-18 09:30:53 +0200

    [feature] 5.X version

--- renderMessage.php
+++ renderMessage.php
@@ -5,7 +5,7 @@
  * @author Denis Chenu <denis@sondages.pro>
  * @copyright 2017-2019 Denis Chenu <http://www.sondages.pro>
  * @license AGPL v3
- * @version 1.3.1
+ * @version 1.4.0
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
